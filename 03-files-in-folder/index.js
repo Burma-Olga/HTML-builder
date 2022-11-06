074b13256f51f169f76d1396470de0e689c3d526
Vadim_Sedelnikov <Burma_Olga@bk.ru>
2022-11-07 00:35:36 +0300

    fix syntax

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -6,16 +6,17 @@ const p = path.join(__dirname + '/secret-folder/');
 async function readFolder(){
   const files = await fs.readdir(p);
   let number = 0;
-   for await (let info of files) {
+  for await (let info of files) {
     const stat = await fs.stat(p + info)
-      if (stat.isFile()) {
-        console.log(
-          path.basename(info, path.extname(info)) + ' - ' + (path.extname(info)).slice(1) + ' - ' + (stat.size / 1024).toFixed(3) + 'kb'
-          );
-        number += 1;
-      }
+    if (stat.isFile()) {
+      console.log(
+        path.basename(info, path.extname(info)) + ' - ' + 
+        (path.extname(info)).slice(1) + ' - ' + 
+        (stat.size / 1024).toFixed(3) + 'kb'
+      );
+      number += 1;
+    }
   }
 }
-
 
 readFolder();
